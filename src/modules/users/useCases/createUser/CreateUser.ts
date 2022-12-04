c35da20ff802592928798293367ad2e4c92dc89d
JFmaia <jfmaia741@gmail.com>
2022-12-04 08:43:19 -0300

    chore: Criando tipagem no dto. Além da criação dos useCases!

---
+++ src/modules/users/useCases/createUser/CreateUser.ts
@@ -0,0 +1,9 @@
+import { CreateUserDTO } from "../../dto/CreateUserDTO";
+
+export class CreateUser{
+   async execute({name, email}:CreateUserDTO) {
+    //verificar se o usuário existe
+
+    //Criar usuário
+   }
+}
