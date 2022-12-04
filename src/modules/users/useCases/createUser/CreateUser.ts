af59ae23681440a3ee6d72e0b4f5adf4fc01d968
JFmaia <jfmaia741@gmail.com>
2022-12-04 10:24:23 -0300

    chore: Criando tabela de relação entre user e filmes.

--- src/modules/users/useCases/createUser/CreateUser.ts
+++ src/modules/users/useCases/createUser/CreateUser.ts
@@ -1,9 +1,29 @@
+import { User } from "@prisma/client";
+import { AppError } from "../../../../errors/AppError";
+import { prisma } from "../../../../prisma/client";
 import { CreateUserDTO } from "../../dto/CreateUserDTO";
 
 export class CreateUser{
-   async execute({name, email}:CreateUserDTO) {
+   async execute({name, email}:CreateUserDTO): Promise<User>{
     //verificar se o usuário existe
+    const userAlreadyExists = await prisma.user.findUnique({
+        where:{
+            email:email
+        }
+    })
 
+    if(userAlreadyExists){
+        throw new AppError("User already exists!",400);
+    }
+    
     //Criar usuário
+    const newUser = await prisma.user.create({
+        data: {
+            name: name,
+            email: email,
+        }
+    });
+
+    return newUser;
    }
 }
