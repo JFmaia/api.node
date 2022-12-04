b42956769d3f33c2e265a789444ba98941334440
JFmaia <jfmaia741@gmail.com>
2022-12-04 10:23:32 -0300

    chore: Adicionando as rotas, habilitando uso de json no express.
    E criando um MiddleWhere de erros, com ele sabemos se o erro que temos é um erro já tratado por nos ou um erro não tratado.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,28 @@
-import express from 'express';
+import "express-async-errors";
+import express, { NextFunction, Request, Response } from 'express';
+import { routes } from './routes';
+import { AppError } from "./errors/AppError";
 
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+// MiddleWhere de erros
+app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
+    //Se o erro ja foi tratado por nos vai mostrar o que foi definido em AppError!
+    if(err instanceof AppError){
+        return response.status(err.statusCode).json({
+            status: "error",
+            message: err.message
+        })
+    }
+
+    // Se o erro não foi tratado por nos vai mostrar o erro em sé!
+    return response.status(500).json({
+        status: "error",
+        message: `Internal Server Error - ${err.message}`,
+    })
+})
 
 app.listen(3333, () => console.log("Server is running in port 3333!"));
