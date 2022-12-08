3c6b374bc7dfdf3d2054f61db212fbb330556d41
JFmaia <jfmaia741@gmail.com>
2022-12-08 16:34:13 -0300

    feat: Rota para get de filmes por data

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreateMovieController } from "../modules/movies/movieCase/createMovie/CreateMovieController";
 import { CreateMovieRentController } from "../modules/movies/movieCase/CreateMovieRent/CreateMovieRentController";
+import { GetMoviesByRealeseDateController } from "../modules/movies/movieCase/getMoviesByRealeseDate/GetMoviesByRealeseDateController";
 
 const createMovieController = new CreateMovieController();
 const createMovieRentController = new CreateMovieRentController();
+const getMoviesByRealeseDate = new GetMoviesByRealeseDateController();
 
 const movieRoutes = Router();
 
 movieRoutes.post("/", createMovieController.handle);
-movieRoutes.post("rent", createMovieRentController.handle);
+movieRoutes.post("/rent", createMovieRentController.handle);
+movieRoutes.get("/release", getMoviesByRealeseDate.handle);
 
 export {movieRoutes};
